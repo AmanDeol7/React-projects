6942280198346d5361141a365ac04ca6a94e8f97
Aman Deol <amandeolm@gmail.com>
2024-05-01 19:01:28 +0530

    adding all files to github

---
+++ src/index.js
@@ -0,0 +1,28 @@
+  import React from 'react';
+  import ReactDOM from 'react-dom/client';
+  import '../node_modules/font-awesome/css/font-awesome.min.css';
+  import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+  import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+  import { Home, Product, Products, AboutPage, ContactPage,  Login, Register } from "./pages"
+
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Products />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+        
+        </Routes>
+
+    </BrowserRouter>
+  );
